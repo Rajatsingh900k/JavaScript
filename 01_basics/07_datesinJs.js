a296c8634884bd6d803404cc7fc2229a00f0a280
Rajatsingh900k <rajatsingh900k@gmail.com>
2024-05-25 23:43:04 +0530

    final commit of 01basics

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,22 +1,28 @@
 // Dates
+//dates in JS are calculated from arbitrary date jan 1 1970
+//dates are ususallly calculated in milliseconds.
 
-let myDate = new Date()
+let myDate = new Date()//object of a date
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON);
+// console.log(myDate.toLocaleTimeString);
+// console.log(myDate.toUTCString);
 // console.log(typeof myDate);
 
+// note:- months in js are started from 0 and not from 1 if months are given in single and not in MM format;
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+let myTimeStamp = Date.now()//give milliseconds from that arbitrary date
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(Math.floor(Date.now()/1000));//to convert from millisec into sec
 
 let newDate = new Date()
 console.log(newDate);
@@ -25,8 +31,9 @@ console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
+let x=newDate.toLocaleString('default', {
     weekday: "long",
-    
+    month:"2-digit",
+    year: "numeric"
 })
-
+console.log(x);
