2de82a4a89f3cbffc85ad45ee4b816234b3f4cfe
Rajatsingh900k <rajatsingh900k@gmail.com>
2024-05-25 14:49:02 +0530

    commit 2.0 fe changes to 01_basics directory
    Upgrading the codes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,6 +8,7 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
+//confusing comparisions
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
@@ -16,6 +17,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === "Strict check: not only compare values but also check for datatype"
 
 console.log("2" === 2);
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-//  Primitive
+//  Primitive:-call by value
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
@@ -9,6 +9,7 @@ const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
+//symbol:-  even if value is same but they are unique.
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
@@ -18,20 +19,25 @@ console.log(id === anotherId);
 
 
 
-// Reference (Non primitive)
+// Reference (Non primitive) :- pass by address/reference
 
 // Array, Objects, Functions
 
+//Array
 const heros = ["shaktiman", "naagraj", "doga"];
+//object(with is {})
 let myObj = {
     name: "hitesh",
     age: 22,
 }
 
+//funtion declaration as variable.
+
+//we can store a funtion in variable in javasript
 const myFunction = function(){
     console.log("Hello world");
 }
-
+console.log(myFunction);
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
